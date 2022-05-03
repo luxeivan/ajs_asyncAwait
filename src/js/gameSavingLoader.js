69c46a2c825ee0caaef2b5bf43a7bd35d56514c7
luxeivan <luxeivan@gmail.com>
2022-05-03 11:22:37 +0300

    completed

--- src/js/gameSavingLoader.js
+++ src/js/gameSavingLoader.js
@@ -3,9 +3,8 @@ import read from './reader';
 
 export default class GameSavingLoader {
   static async load() {
-    console
     const data = await read();
     const string = await json(data);
-    return JSON.parse(string)
+    return JSON.parse(string);
   }
 }
